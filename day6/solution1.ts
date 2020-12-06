ada0c596909dd107362938f285c57212b2217f1a
Anna Moshkina <a.v.moshkina@gmail.com>
2020-12-06 08:03:59 +0100

    Day 6, part 1

---
+++ day6/solution1.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+
+
+class Solver {
+    run(filename: string) {
+        const groups: string[] = fs
+            .readFileSync(filename, 'utf-8')
+            .split('\n\n');
+        
+        let counter = 0;
+        for (const group of groups) {
+            const uniques: Set<string> = new Set();
+            for (const person of group.split('\n')) {
+                person.split('').forEach(
+                    ch => uniques.add(ch)
+                );
+            }
+            // console.log(uniques)
+            counter += uniques.size;
+        }
+
+        console.log(`Answer: ${counter}`);
+    }
+}
+
+new Solver().run('./day6/input.txt')
