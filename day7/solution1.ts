0cbc89c6ed6010d48944092ad6a933ce8426aca8
Anna Moshkina <a.v.moshkina@gmail.com>
2020-12-10 22:07:17 +0100

    Day 7: part 1 working draft

--- day7/solution1.ts
+++ day7/solution1.ts
@@ -18,14 +18,6 @@ class Solver {
 
     keyBag = 'shiny gold';
 
-    printTree(bagTree: Map<string, BagNode>) {
-        for (const [name, node] of bagTree.entries()) {
-            console.log('!-----!');
-            console.log(name);
-            console.log(node.children);
-        }
-    }
-
     getBagPtr(bagName: string, bagLookup: Map<string, BagNode>): BagNode {
         // creating a new node for the given bag and adding it to the lookup
         if (!bagLookup.has(bagName)) {
@@ -43,7 +35,6 @@ class Solver {
         const allChildren: Set<string> = new Set();
 
         for (const rule of rules) {
-            // console.log(rule);
             const [outerBag, innerBags] = rule.split(' bags contain ');
             
 
@@ -72,30 +63,22 @@ class Solver {
     }
 
     traverseBagTree(queue: QueueItem[], visited: Set<string>, leads: Set<string>) {
-        // let result: Set<string> = new Set();
         while (queue.length > 0) {
             const item = queue.pop();
             const [node, path] = [item.node, item.path];
-            // console.log('\n');
-            // console.log('Got node: ', node);
 
             if (node.bagName == this.keyBag || leads.has(node.bagName)) {
-                // console.log('got to leads', leads);
-                // result = new Set([...result, ...path]);
                 leads = new Set([...leads, ...path]);
                 continue;
             }
 
             if (visited.has(node.bagName)) {
-                // console.log('got to visited');
                 continue;
             }
             visited.add(node.bagName);
 
             for (const child of node.children) {
-                // console.log('Got child: ', child);
                 if (node.bagName == this.keyBag || leads.has(node.bagName)) {
-                    // result = new Set([...result, ...path]);
                     leads = new Set([...leads, ...path]);
                 }
                 const pathClone: Set<string> = new Set(path);
@@ -116,28 +99,17 @@ class Solver {
 
         const visited: Set<string> = new Set();
         let leads: Set<string> = new Set();
-        // leads.add(this.keyBag);
 
-        // let result: Set<string> = new Set();
         for (const node of bagLookup.values()) {
-            // if (visited.has(node.bagName)) {
-            //     continue;
-            // }
 
             if (!node.isRoot) {
                 continue;
             }
 
-            // console.log('Starting traversing from: ', node);
-            // result = new Set([...result, ...this.traverseBagTree([{node, path: new Set()}], visited, leads)]);
-
             leads = this.traverseBagTree([{node, path: new Set()}], visited, leads)
         }
-        // console.log(`visited size: ${visited.size}, visited: `)
-        // for (const v of visited.values()) {
-        //     console.log(v, v.length)
-        // }
-        console.log(`Answer: ${leads.size}`, leads);
+
+        console.log(`Answer: ${leads.size}`);
     }
 }
 
